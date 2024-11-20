a94226160793f6d11900a7e62c13304cdf7e8ce7
Bezyl Mophat Otieno <bezylmophatotieno@gmail.com>
2024-11-20 07:35:57 +0300

    Added more services in the portfolio section

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -10,11 +10,11 @@ const projects = [
   {
     id: 1,
     title: "M-Pesa Integration Platform",
-    description: "A comprehensive payment integration system for East African markets, handling over 1M transactions daily.",
+    description: "We did support Product Trace Limited with a M-Pesa integration platform. This platform was developed using the M-Pesa API and integrated with the M-Pesa mobile app. The platform provided a seamless experience for users to manage their M-Pesa accounts, track their transactions, and receive notifications for their account balance.",
     date: "December 2023",
     videoUrl: "https://youtu.be/1y7bv8FN5Wo?si=xr-za3mkuLBappEx",
     thumbnail: "https://images.unsplash.com/photo-1563013544-824ae1b704d3?auto=format&fit=crop&q=80&w=400",
-    projectUrl: "https://project1.example.com",
+    projectUrl: "https://www.product-traceltd.com/",
   },
   {
     id: 2,
@@ -25,14 +25,32 @@ const projects = [
     thumbnail: "https://images.unsplash.com/photo-1586771107445-d3ca888129ff?auto=format&fit=crop&q=80&w=400",
     projectUrl: "https://project2.example.com",
   },
-  {
+    {
     id: 3,
+    title: "Computer Clinic 254",
+    description: "Computer Clinic 254 is a computer repair and maintenance company that provides services to individuals and businesses in Western Africa. The company has a team of highly skilled technicians who are dedicated to providing high-quality repairs and maintenance services to its clients. The company has a strong focus on customer satisfaction and has won several awards for its services.",
+    date: "October 2024",
+    videoUrl: "https://youtu.be/C-wowZSZIgM?si=XJHw0sslfYAM1CYI",
+    thumbnail: "https://img.freepik.com/free-photo/people-repairing-computer-chips_23-2150880951.jpg?t=st=1732077038~exp=1732080638~hmac=46447cf3f721f757473255d6bd1deb54f295bf50bbcadf7c1519b6a7bbb783ac&w=826",
+    projectUrl: "computer-clinic254.vercel.app",
+  },
+    {
+    id: 4,
+    title: "Rico Technologies Limited",
+    description: "Rico Technologies is an Ecommerce Company specialized in selling electronics and gadgets. The company has a wide range of products, including smartphones, laptops, and other electronic devices. Rico Technologies has a strong focus on customer satisfaction and has won several awards for its services.",
+    date: "September 2024",
+    videoUrl: "https://youtu.be/C-wowZSZIgM?si=XJHw0sslfYAM1CYI",
+    thumbnail: "https://img.freepik.com/free-photo/industrial-designers-working-3d-model_23-2149370123.jpg?t=st=1732077103~exp=1732080703~hmac=162cef3f7e99ef0aec04bb6f559dc2f2ac8c24b70ce7d4de731978e5c4b65c3a&w=826",
+    projectUrl: "rico-technologies-ltd.vercel.app",
+  },
+  {
+    id: 5,
     title: "EduTech Learning Platform",
     description: "Mobile-first educational platform serving 200K+ students across African universities.",
     date: "September 2023",
     videoUrl: "https://youtu.be/U9tR3ytxL-o?si=AI0aRCwWYN4SKbvo",
     thumbnail: "https://images.unsplash.com/photo-1610484826967-09c5720778c7?auto=format&fit=crop&q=80&w=400",
-    projectUrl: "https://project3.example.com",
+    projectUrl: "https://teach2give.com/",
   },
 ];
 
